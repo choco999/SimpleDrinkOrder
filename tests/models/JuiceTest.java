f2b3025659110f27e16bda274ba36ef09b2d308a
Chisato <49408516+choco999@users.noreply.github.com>
2021-04-03 16:17:53 -0400

    Updated dashboardview.fxml

---
+++ tests/models/JuiceTest.java
@@ -0,0 +1,31 @@
+package models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class JuiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getIngredients() {
+    }
+
+    @Test
+    void addIngredient() {
+    }
+
+    @Test
+    void hasPulp() {
+    }
+
+    @Test
+    void setHasPulp() {
+    }
+
+    @Test
+    void getNumOfIngredients() {
+    }
+}
